05a1915906a2b50b958e10c4e7fe9b4abf8654f0
Emre Ozdemir <emreozdemir@gmail.com>
2017-04-17 12:47:11 -0700

    defined isValidWalk() function

--- python_practice.py
+++ python_practice.py
@@ -22,8 +22,42 @@ def sum_mutiple_5and3(number):
 #Descending Order
 def Descending_Order(num):
     """
+    ARGS: num (int)
+    RETURNS : int() decending ordered num
+
     Your task is to make a function that can take any non-negative integer
     as a argument and return it with it's digits in descending order. Essentially,
     rearrange the digits to create the highest possible number.
     """
     return int(''.join(sorted(list(str(num)),reverse=True)))
+
+# Take a Ten Minute Walk
+def isValidWalk(walk):
+    #determine if walk is valid
+    """
+    ARGS : walk (list) list of strings such as ["n","w","e","s"]
+    RETURNS : boolean "True" if the given directions is 10 minutes walk
+
+    You live in the city of Cartesia where all roads are laid out in a perfect grid.
+    You arrived ten minutes too early to an appointment, so you decided to take
+    the opportunity to go for a short walk. The city provides its citizens with
+    a Walk Generating App on their phones -- everytime you press the button it
+    sends you an array of one-letter strings representing directions to walk
+    (eg. ['n', 's', 'w', 'e']). You know it takes you one minute to traverse
+    one city block, so create a function that will return true if the walk
+    the app gives you will take you exactly ten minutes (you don't want to be
+    early or late!) and will, of course, return you to your starting point.
+    Return false otherwise.
+
+    """
+    walkdict={"n":1,"s":-1,"e":2,"w":-2}
+    walklist=[]
+    if len(walk)!=10:
+        return false
+    else:
+        for w in walk:
+            walklist.append(walkdict[w])
+        if sum(walklist)==0:
+            return True
+        else:
+            return False
