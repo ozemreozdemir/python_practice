12ba5fb8580533b3eed15c383611618857e84e78
Emre Ozdemir <emreozdemir@gmail.com>
2017-05-07 13:45:45 -0700

    defined miniongame() function

--- python_practice.py
+++ python_practice.py
@@ -229,3 +229,45 @@ def nbr_of_laps(x, y):
         int_ratio=Fraction(laprate).limit_denominator()
         laplist=[int_ratio.denominator,int_ratio.numerator]
     return laplist
+
+
+def minion_game(string):
+    """
+    Kevin and Stuart want to play the 'The Minion Game'.
+    Game Rules
+
+    Both players are given the same string, .
+    Both players have to make substrings using the letters of the string .
+    Stuart has to make words starting with consonants.
+    Kevin has to make words starting with vowels.
+    The game ends when both players have made all possible substrings.
+
+    Scoring
+    A player gets +1 point for each occurrence of the substring in the string .
+
+    For Example:
+    String  = BANANA
+    Kevin's vowel beginning word = ANA
+    Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+    """
+    mdict={}
+    for char in list(set(string)):
+        for i in range(len(string)):
+            for j in range(len(string)+1):
+                word = char+string[i:j]
+                if word not in mdict:
+                    mdict[word]=string.count(word)
+    s=0
+    k=0
+    for word in mdict:
+        if word in string:
+            if word[0] in list('aeiouAEIOU'):
+                k+=mdict[word]
+            else:
+                s+=mdict[word]
+    if s > k:
+        print "Stuart", s
+    elif k > s:
+        print "Kevin" , k
+    else:
+        print 'Draw'
